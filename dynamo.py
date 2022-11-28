63958051c3c63395ebf989d7c6a9325f9c74b301
Silvio Lugaro <silvio.lugaro@gruppoiren.it>
2022-11-28 08:39:31 +0100

    feat: general improvement

--- dynamo.py
+++ dynamo.py
@@ -1,12 +1,11 @@
 from functools import wraps
-from typing import Any, List, Mapping, Optional, Sequence
+from typing import Any, Mapping, Optional, Sequence
 
 import awswrangler as wr
 import boto3
 import pandas as pd
-from awswrangler import _utils
+from awswrangler import _utils, exceptions
 from boto3.dynamodb.conditions import ConditionBase
-from boto3.dynamodb.table import TableResource
 from boto3.dynamodb.transform import TypeDeserializer, TypeSerializer
 from botocore.exceptions import ClientError
 from loguru import logger
@@ -40,53 +39,67 @@ def handle_boto3_client_error(func):
 
 
 @handle_boto3_client_error
-def _read_items(table: TableResource,
+def _read_items(table_name: str,
                 boto3_session: Optional[boto3.Session] = None,
                 **kwargs
                 ) -> Sequence:
+    
+    # Get DynamoDB client and Table instance
     client = _utils.client(service_name='dynamodb', session=boto3_session)
+    table = wr.dynamodb.get_table(table_name=table_name, boto3_session=boto3_session)
+
+    # Extract 'Keys' from provided kwargs
     keys = kwargs.pop('Keys', None)
-    if keys is not None:
-        if len(keys) == 1:
-            logger.debug('get_item')
-            return [
-                table
-                .get_item(Key=keys[0], **kwargs)
-                .get('Item', dict())
-            ]
-        else:
-            logger.debug('batch_get_item')
-            # TODO: manage UnprocessedKeys 
-            ...
-            ts, tds = TypeSerializer(), TypeDeserializer()
-            items = (
-                    client.batch_get_item(
-                        RequestItems={
-                            table.table_name: {
-                                'Keys': list(map(lambda x: {k: ts.serialize(v) for k, v in x.items()}, keys)),
-                                **kwargs
-                            }
-                        }
-                    )
-                    ['Responses']
-                    [table.table_name]
+
+    # Conditionally set reading method
+    use_get_item = (keys is not None) and (len(keys)==1)
+    use_batch_get_item = (keys is not None) and (len(keys) > 1)
+    use_query = (keys is None) and ('KeyConditionExpression' in kwargs)
+    use_scan = (keys is None) and ('KeyConditionExpression' not in kwargs)
+
+    # Read items
+    if use_get_item:
+        logger.debug('get_item')
+        kwargs['Key'] = keys[0]
+        items = [
+            table
+            .get_item(**kwargs)
+            .get('Item', {})
+        ]
+    elif use_batch_get_item:
+        logger.debug('batch_get_item')
+        # TODO: manage UnprocessedKeys 
+        ...
+        ts, tds = TypeSerializer(), TypeDeserializer()
+        kwargs['Keys'] = list(map(
+            lambda x: {k: ts.serialize(v) for k, v in x.items()},
+            keys
+        ))
+        items = list(
+            map(
+                lambda x: {k: tds.deserialize(v) for k, v in x.items()},
+                client.batch_get_item(RequestItems={table_name: kwargs})
+                .get('Responses', {table_name: []})
+                .get(table_name, [])
             )
-            return list(map(lambda x: {k: tds.deserialize(v) for k, v in x.items()}, items))
-    else:
-        if 'KeyConditionExpression' in kwargs:
+        )
+    elif use_query or use_scan:
+        if use_query:
             logger.debug('query')
-            return (
-                table
-                .query(**kwargs)
-                ['Items']
-            )
         else:
             logger.debug('scan')
-            return (
-                table
-                .scan(**kwargs)
-                .get('Items')
-            )
+
+        _read_method = table.query if use_query else table.scan
+        response = _read_method(**kwargs)
+        items = response.get('Items', [])
+        
+        # Handle pagination
+        while 'LastEvaluatedKey' in response:
+            kwargs['ExclusiveStartKey'] = response['LastEvaluatedKey']
+            response = _read_method(**kwargs)
+            items.extend(response.get('Items', []))
+    
+    return items
 
 
 def read_items(*,
@@ -104,27 +117,28 @@ def read_items(*,
                boto3_session: Optional[boto3.Session] = None
                ) -> Sequence:
 
-    table = wr.dynamodb.get_table(table_name=table_name, boto3_session=boto3_session)    
-
-    # Get table key schema
-    key_schema = table.key_schema
+    table_key_schema = (
+        wr.dynamodb
+        .get_table(table_name=table_name, boto3_session=boto3_session)
+        .key_schema
+    )
 
     # Detect sort key, if any
-    if len(key_schema) == 1:
-        partition_key, sort_key = key_schema[0]['AttributeName'], None
+    if len(table_key_schema) == 1:
+        partition_key, sort_key = table_key_schema[0]['AttributeName'], None
     else:
         partition_key, sort_key = (
-            next(filter(lambda x: x['KeyType'] == 'HASH', table.key_schema))[
+            next(filter(lambda x: x['KeyType'] == 'HASH', table_key_schema))[
                 'AttributeName'],
-            next(filter(lambda x: x['KeyType'] == 'RANGE', table.key_schema))[
+            next(filter(lambda x: x['KeyType'] == 'RANGE', table_key_schema))[
                 'AttributeName']
         )
 
     def ensure_coherency():
         if sort_values is None:
-            raise ValueError(f'Table {table_name} has {sort_key} as sort key: please provide sort_values.')
+            raise exceptions.InvalidArgumentType(f'Argument sort_values cannot be None: table {table_name} has {sort_key} as sort key.')
         elif len(sort_values) != len(partition_values):
-            raise ValueError('Partition and sort values must have the same length.')
+            raise exceptions.InvalidArgumentCombination('Partition and sort values must have the same length.')
 
     # Build kwargs shared by read methods
     kwargs = {
@@ -152,9 +166,9 @@ def read_items(*,
         kwargs['Limit'] = max_items_evaluated
         
     if ('Keys' in kwargs) or ('KeyConditionExpression' in kwargs) or ('FilterExpression' in kwargs) or allow_full_scan or max_items_evaluated:
-        items = _read_items(table, boto3_session, **kwargs)
+        items = _read_items(table_name, boto3_session, **kwargs)
     else:
-        raise ValueError(
-            f'Please provide at least one between partition_values, sort_values and filter_expression or allow full scan.')
+        raise exceptions.InvalidArgumentCombination(
+            f'Please provide at least one between partition_values, sort_values, filter_expression, allow_full_scan or max_items_evaluated.')
 
     return pd.DataFrame(items)
